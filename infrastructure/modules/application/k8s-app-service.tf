28c953049d8c45ca66106fc89eaf4a7cd5e1adee
Mikhail Pazniak <mpazniak@Mikhails-MBP.lan>
2022-09-30 01:38:16 +0300

    issue/#30: Fixed service API version

--- infrastructure/modules/application/k8s-app-service.tf
+++ infrastructure/modules/application/k8s-app-service.tf
@@ -1,4 +1,4 @@
-resource "kubernetes_service" "this" {
+resource "kubernetes_service_v1" "this" {
   metadata {
     name      = "${var.environment}-${var.application_name}-ingress-service"
     namespace = "${var.environment}-${var.application_name}-namespace"
@@ -8,12 +8,13 @@ resource "kubernetes_service" "this" {
       app = kubernetes_deployment.application.metadata.0.labels.app
       env = var.environment
     }
-
+    session_affinity = "ClientIP"
     port {
       port        = var.app_host_port
       target_port = var.app_container_port
       protocol    = "TCP"
     }
-    type = "NodePort"
+
+    type = "LoadBalancer"
   }
 }
