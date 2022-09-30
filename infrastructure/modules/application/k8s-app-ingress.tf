1808d2469f446d8267289c8f0aa12a298bbb5041
Mikhail Pazniak <mikhail.pazniak@boweryvaluation.com>
2022-09-30 10:25:16 +0300

    hotfix/ingress: Force changed 'wait_for_load_balancer' ingress parameter in main branch

--- infrastructure/modules/application/k8s-app-ingress.tf
+++ infrastructure/modules/application/k8s-app-ingress.tf
@@ -1,5 +1,5 @@
 resource "kubernetes_ingress_v1" "this" {
-  wait_for_load_balancer = false
+  wait_for_load_balancer = true
   metadata {
     name      = "${var.environment}-${var.application_name}-ingress"
     namespace = "${var.environment}-${var.application_name}-namespace"
