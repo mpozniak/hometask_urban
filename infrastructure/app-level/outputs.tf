c2faa78b9eec33754088652ac7d77e0ea721fdee
Mikhail Pazniak <mpazniak@Mikhails-MacBook-Pro.local>
2022-09-30 10:45:47 +0300

    issue/#30: Deleted app module output 'load_balancer_hostname'

--- infrastructure/app-level/outputs.tf
+++ infrastructure/app-level/outputs.tf
@@ -1,8 +1,3 @@
-output "load_balancer_hostname" {
-  value       = module.urban_app.load_balancer_hostname
-  description = "Load Balancer hostname"
-}
-
 output "load_balancer_ip" {
   value       = module.urban_app.load_balancer_ip
   description = "Load Balancer hostname"
--- infrastructure/modules/application/outputs.tf
+++ infrastructure/modules/application/outputs.tf
@@ -1,8 +1,3 @@
-# Display load balancer hostname
-output "load_balancer_hostname" {
-  value = kubernetes_ingress_v1.this.status.0.load_balancer.0.ingress.0.hostname
-}
-
 # Display load balancer IP
 output "load_balancer_ip" {
   value = kubernetes_ingress_v1.this.status.0.load_balancer.0.ingress.0.ip
